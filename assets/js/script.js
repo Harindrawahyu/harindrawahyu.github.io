5efc3d49c22d8ec460a644e7d7e3fe0bcd4c024c
harindra <harindrawahyu131@gmail.com>
2024-12-23 17:03:16 +0700

    update form work, js (3)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
-const form = document.getElementById('form');
-            form.addEventListener('submit', function(e) {
-                e.preventDefault(); // Prevent default form submission
-                const formData = new FormData(form);
-                fetch(form.action, {
-                    method: 'POST',
-                    body: formData,
-                    headers: {
-                        'Accept': 'application/json'
-                    }
-                }).then(response => {
-                    if (response.ok) {
-                        Swal.fire('Success!', 'Your message has been sent.', 'success');
-                        form.reset();
-                    } else {
-                        Swal.fire('Oops!', 'Something went wrong. Please try again.', 'error');
-                    }
-                }).catch(error => {
-                    Swal.fire('Oops!', 'Something went wrong. Please try again.', 'error');
-                });
-            });
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function () {
+
+const name = document.getElementById('name').value; 
+const email = document.getElementById('email').value; 
+const pesan = document.getElementById('pesan').value;
+          
+    if( name == '' || email == '' || pesan == '') {
+       
+       Swal.fire(
+          'Form kosong',
+          'Harap isi form yg kosong',
+          'warning'
+        )  
+                    
+    } else {              
+       
+         Swal.fire(
+        'Terima Kasih!',
+        'Pesan anda telah diterima!',
+        'success'
+    )        
+    }
+});    
